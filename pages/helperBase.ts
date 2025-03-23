19de3870d6fc1272d27248a23d473a0ccfd48d0c
AzureAD\TomášRambousekTesena <Tomas_rambousek@tesena.cz>
2025-03-23 16:48:54 +0100

    Komentáře

--- pages/helperBase.ts
+++ pages/helperBase.ts
@@ -1,13 +1,18 @@
 import { Page } from "@playwright/test";
 
+// Základní třída HelperBase, která poskytuje společné metody pro práci se stránkou
 export class HelperBase {
-  readonly page: Page;
+  readonly page: Page; // Proměnná pro uchování instance stránky Playwrightu
 
   constructor(page: Page) {
-    this.page = page;
+    this.page = page; // Inicializace instance stránky
   }
 
+  /**
+   * Metoda pro čekání po zadaný počet sekund
+   * @param timeInSeconds - Počet sekund, po které má test čekat
+   */
   async waitForNumberOfSeconds(timeInSeconds: number) {
-    await this.page.waitForTimeout(timeInSeconds * 1000);
+    await this.page.waitForTimeout(timeInSeconds * 1000); // Převod sekund na milisekundy a čekání
   }
 }
--- pages/pageManager.ts
+++ pages/pageManager.ts
@@ -3,27 +3,37 @@ import { NavigationPage } from "./navigationPage";
 import { FormLayoutPage } from "./formLayoutPage";
 import { DatepickerPage } from "./datePickerPage";
 
+// Třída PageManager slouží jako správce stránek a poskytuje přístup k jednotlivým stránkám aplikace
 export class PageManager {
-  private readonly page: Page;
-  private readonly navigationPage: NavigationPage;
-  private readonly formLayoutPage: FormLayoutPage;
-  private readonly datepickerPage: DatepickerPage;
+  private readonly page: Page; // Instance stránky Playwrightu
+  private readonly navigationPage: NavigationPage; // Instance třídy NavigationPage
+  private readonly formLayoutPage: FormLayoutPage; // Instance třídy FormLayoutPage
+  private readonly datepickerPage: DatepickerPage; // Instance třídy DatepickerPage
 
   constructor(page: Page) {
-    this.page = page;
-    this.navigationPage = new NavigationPage(this.page);
-    this.formLayoutPage = new FormLayoutPage(this.page);
-    this.datepickerPage = new DatepickerPage(this.page);
+    this.page = page; // Inicializace instance stránky
+    this.navigationPage = new NavigationPage(this.page); // Inicializace navigační stránky
+    this.formLayoutPage = new FormLayoutPage(this.page); // Inicializace stránky s formuláři
+    this.datepickerPage = new DatepickerPage(this.page); // Inicializace stránky s DatePickerem
   }
 
+  /**
+   * Vrací instanci NavigationPage pro práci s navigací
+   */
   navigateTo() {
     return this.navigationPage;
   }
 
+  /**
+   * Vrací instanci FormLayoutPage pro práci s formuláři
+   */
   onFormLayoutsPage() {
     return this.formLayoutPage;
   }
 
+  /**
+   * Vrací instanci DatepickerPage pro práci s DatePickerem
+   */
   onDatePickerPage() {
     return this.datepickerPage;
   }
